efb1906fce8a35d7770dc9d51d87a150c9796a6c
Rails Udemy Course <jjdrill@hotmail.com>
2018-08-19 11:52:38 -0600

    base lane updated with hero and base cards

--- spec/models/base_lane_spec.rb
+++ spec/models/base_lane_spec.rb
@@ -2,11 +2,37 @@ require "./app/models/base_lane"
 
 describe Base_Lane do
 
-  before(:all) do
+  before(:each) do
     @lane = Base_Lane.new
+    @fake_base = Base_Card.new(
+      Faker::String.random(20),
+      Faker::String.random(50)
+    )
+    @fake_hero_1 = Hero_Card.new(
+      Faker::StarWars.character,
+      Faker::StarWars.quote,
+      Faker::Number.number(2),
+      Faker::Number.number(2)
+    )
+    @fake_hero_2 = Hero_Card.new(
+      Faker::StarWars.character,
+      Faker::StarWars.quote,
+      Faker::Number.number(2),
+      Faker::Number.number(2)
+    )
+    @fake_hero_3 = Hero_Card.new(
+      Faker::StarWars.character,
+      Faker::StarWars.quote,
+      Faker::Number.number(2),
+      Faker::Number.number(2)
+    )
   end
 
   let(:lane) { @lane }
+  let(:fake_base) { @fake_base }
+  let(:fake_hero_1) { @fake_hero_1 }
+  let(:fake_hero_2) { @fake_hero_2 }
+  let(:fake_hero_3) { @fake_hero_3 }
 
   it "checks initial state" do
     expect(lane.base).to eq(nil)
@@ -14,16 +40,17 @@ describe Base_Lane do
   end
 
   it "checks add_base" do
-    lane.add_base("Test Base")
-    expect(lane.base).to eq("Test Base")
+    lane.add_base(fake_base)
+    expect(lane.base).to eq(fake_base)
   end
 
   it "checks remove_base" do
-    lane.add_base("Test Base")
-    lane.add_knight("Test Knight 1", 0)
-    lane.add_knight("Test Knight 2", 1)
-    lane.add_knight("Test Knight 3", 2)
-    expect(lane.base).to eq("Test Base")
+    lane.add_base(fake_base)
+    expect(lane.base).to eq(fake_base)
+    lane.add_knight(fake_hero_1, 0)
+    lane.add_knight(fake_hero_2, 1)
+    lane.add_knight(fake_hero_3, 2)
+
     lane.remove_base
     expect(lane.base).to eq(nil)
     expect(lane.knights[0]).to eq(nil)
@@ -32,32 +59,32 @@ describe Base_Lane do
   end
 
   it "checks add knight" do
-    lane.add_knight("Test Knight 1", 0)
-    lane.add_knight("Test Knight 2", 1)
-    lane.add_knight("Test Knight 3", 2)
-    expect(lane.knights[0]).to eq("Test Knight 1")
-    expect(lane.knights[1]).to eq("Test Knight 2")
-    expect(lane.knights[2]).to eq("Test Knight 3")
+    lane.add_knight(fake_hero_1, 0)
+    lane.add_knight(fake_hero_2, 1)
+    lane.add_knight(fake_hero_3, 2)
+    expect(lane.knights[0]).to eq(fake_hero_1)
+    expect(lane.knights[1]).to eq(fake_hero_2)
+    expect(lane.knights[2]).to eq(fake_hero_3)
   end
 
   it "checks remove knight" do
-    lane.add_knight("Test Knight 1", 0)
-    lane.add_knight("Test Knight 2", 1)
-    lane.add_knight("Test Knight 3", 2)
-    expect(lane.knights[0]).to eq("Test Knight 1")
-    expect(lane.knights[1]).to eq("Test Knight 2")
-    expect(lane.knights[2]).to eq("Test Knight 3")
+    lane.add_knight(fake_hero_1, 0)
+    lane.add_knight(fake_hero_2, 1)
+    lane.add_knight(fake_hero_3, 2)
+    expect(lane.knights[0]).to eq(fake_hero_1)
+    expect(lane.knights[1]).to eq(fake_hero_2)
+    expect(lane.knights[2]).to eq(fake_hero_3)
 
     removed_card = lane.remove_knight(0)
     expect(lane.knights[0]).to eq(nil)
-    expect(removed_card).to eq("Test Knight 1")
+    expect(removed_card).to eq(fake_hero_1)
 
     removed_card = lane.remove_knight(1)
     expect(lane.knights[1]).to eq(nil)
-    expect(removed_card).to eq("Test Knight 2")
+    expect(removed_card).to eq(fake_hero_2)
 
     removed_card = lane.remove_knight(2)
     expect(lane.knights[2]).to eq(nil)
-    expect(removed_card).to eq("Test Knight 3")
+    expect(removed_card).to eq(fake_hero_3)
   end
 end
