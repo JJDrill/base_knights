d6935f26f10c1822a7b4b43ac72e1c3a58b921a2
Rails Udemy Course <jjdrill@hotmail.com>
2018-08-19 11:19:31 -0600

    add hero card tests added and passing

--- spec/models/hero_card_spec.rb
+++ spec/models/hero_card_spec.rb
@@ -1,9 +1,25 @@
 require './app/models/hero_card'
+require 'Faker'
 
 describe Hero_Card do
 
+  before(:all) do
+    @fake_name = Faker::StarWars.character
+    @fake_description = Faker::StarWars.quote
+    @fake_attack = Faker::Number.number(2)
+    @fake_defense = Faker::Number.number(2)
+  end
+
+  let(:fake_name) { @fake_name }
+  let(:fake_description) { @fake_description }
+  let(:fake_attack) { @fake_attack }
+  let(:fake_defense) { @fake_defense }
+
   it "checks a new hero card initialize" do
-    @hero = Hero_Card.new("HeroName", "Hero Description", 123, 543)
-    # expect(@hero.name).to eq("HeroName")
+    @hero = Hero_Card.new(fake_name, fake_description, fake_attack, fake_defense)
+    expect(@hero.name).to eq(fake_name)
+    expect(@hero.description).to eq(fake_description)
+    expect(@hero.attack).to eq(fake_attack)
+    expect(@hero.defense).to eq(fake_defense)
   end
 end
