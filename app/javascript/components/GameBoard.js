92bf7ba09771ea4e4c63874e743f4278c7f6f51c
Rails Udemy Course <jjdrill@hotmail.com>
2018-08-26 10:32:11 -0600

    added GameBoard component

---
+++ app/javascript/components/GameBoard.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class GameBoard extends React.Component {
+  render () {
+    return (
+      <div className="GameBoard">
+      </div>
+    );
+  }
+}
+
+export default GameBoard
