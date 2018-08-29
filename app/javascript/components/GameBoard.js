bfda08d2ab49a6b030daeddf768457a8b6ebec72
Rails Udemy Course <jjdrill@hotmail.com>
2018-08-28 19:05:13 -0600

    added lane component and other game board updates

--- app/javascript/components/GameBoard.js
+++ app/javascript/components/GameBoard.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Lane from "./Lane"
 
 class GameBoard extends React.Component {
   render () {
     return (
       <div className="GameBoard">
+        <Lane />
+        <Lane />
+        <Lane />
       </div>
     );
   }
---
+++ app/javascript/components/Lane.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+import BaseCard from "./BaseCard"
+
+class Lane extends React.Component {
+  render () {
+    return (
+      <div className="Lane">
+        <BaseCard />
+      </div>
+    );
+  }
+}
+
+export default Lane
